aee4e20c875bd9bbd61b71deba2571f6dafca049
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-14 13:38:34 +0300

    add models

--- photos/models.py
+++ photos/models.py
@@ -10,3 +10,28 @@ class User(models.Model):
 
     def __str__(self):
         return {self.first_name}
+
+
+class ImageCategory(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return {self.name}
+
+
+class ImageLocation(models.Model):
+    location_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return {self.location_name}
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    image_name = models.CharField(max_length=10)
+    image_description = models.TextField()
+    image_location = models.ForeignKey(ImageLocation, on_delete=models.CASCADE)
+    image_category = models.ForeignKey(ImageCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return {self.image_name}
