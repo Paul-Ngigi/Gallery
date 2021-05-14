d6589fc866d2a7d767789f6b38df93480cfccb9b
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-14 12:42:02 +0300

    add a user model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return {self.first_name}
