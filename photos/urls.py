e511a82464c77835aa834dc91cf09543c6098d11
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-17 14:40:19 +0300

    update the urls

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search_results, name='search_results'),
+    re_path('image/(?P<image_id>\d+)', views.view_image, name='image'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
