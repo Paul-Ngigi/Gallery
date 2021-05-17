30abc26c33ebde0aa590ee058ec308c2a85df1f7
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-17 14:39:46 +0300

    add a view image view

--- photos/views.py
+++ photos/views.py
@@ -13,7 +13,8 @@ def index(request):
 def search_results(request):
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
-        searched_images = Image.search_image(search_term)
+        categories = Image.objects.get(name=search_term)
+        searched_images = Image.search_image(categories)
         message = f"{search_term}"
 
         return render(request, 'search.html', {'message': message, 'images': searched_images})
@@ -26,9 +27,9 @@ def search_results(request):
 def view_image(request, image_id):
     try:
         image = Image.objects.get(id=image_id)
-        return image
+        return render(request, 'image.html', {'image': image})
 
     except ObjectDoesNotExist:
         message = 'Sorry, we could not find what you are looking for'
-        return render(request, 'image.html', {'image': image})
+        return render(request, 'image.html', {'message': message})
 
