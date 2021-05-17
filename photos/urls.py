7f224da18c0e71d1c11180df5046bf100dd8b2fd
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-17 17:19:53 +0300

    add a url

--- photos/urls.py
+++ photos/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search_results, name='search_results'),
     re_path('image/(?P<image_id>\d+)', views.view_image, name='image'),
+    re_path('category/(?P<category_id>\d+)', views.get_category, name='category'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
